57287adea0cb57462ee133162a3660ab6195d4b5
gxr404 <1650542259@qq.com>
2024-06-10 15:07:48 +0800

    chore: update doc

--- src/utils/wagmi_config.ts
+++ src/utils/wagmi_config.ts
@@ -3,8 +3,6 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { bscTestnet, sepolia } from 'wagmi/chains'
 
-// 9160a3b31a3d70aa7c4e8d0bfe95038c
-
 export const config = getDefaultConfig({
   appName: 'web3 demo',
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
